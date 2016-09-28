0a4346cd3b8216a571da828e5e03bfb6dc4ec8be
masterUNG <phrombutr@gmail.com>
2016-09-28 11:54:33 +0700

    ListView Finish

--- app/src/main/java/appewtc/masterung/sortingthai/SortListView.java
+++ app/src/main/java/appewtc/masterung/sortingthai/SortListView.java
@@ -2,12 +2,27 @@ package appewtc.masterung.sortingthai;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class SortListView extends AppCompatActivity {
+
+    //Explicit
+    private ListView listView;
+    private String[] strings;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sort_list_view);
-    }
-}
+
+        listView = (ListView) findViewById(R.id.sortListview);
+        strings = getResources().getStringArray(R.array.soft);
+
+        // Create ListView
+        SortAdapter sortAdapter = new SortAdapter(this, strings);
+        listView.setAdapter(sortAdapter);
+
+    }   // Main Method
+
+}   // Main Class
