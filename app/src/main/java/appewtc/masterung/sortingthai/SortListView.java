a07ab69cc15f109829aebd0415fa54d1b9881f77
masterUNG <phrombutr@gmail.com>
2016-09-28 13:11:45 +0700

    Intent to SortTest finsish

--- app/src/main/java/appewtc/masterung/sortingthai/SortListView.java
+++ app/src/main/java/appewtc/masterung/sortingthai/SortListView.java
@@ -1,7 +1,10 @@
 package appewtc.masterung.sortingthai;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class SortListView extends AppCompatActivity {
@@ -22,6 +25,17 @@ public class SortListView extends AppCompatActivity {
         // Create ListView
         SortAdapter sortAdapter = new SortAdapter(this, strings);
         listView.setAdapter(sortAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Intent intent = new Intent(SortListView.this, SortTest.class);
+                intent.putExtra("Index", i);
+                startActivity(intent);
+
+            }
+        });
 
     }   // Main Method
 
